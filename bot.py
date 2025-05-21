edf56950e2a55c5f29426264c2e046cb4916ec05
k1berteftel <kkulis985@gmail.com>
2025-05-22 02:22:36 +0300

    fix

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ async def run_uvicorn():
     app = FastAPI()
     app.include_router(router)
 
-    config = uvicorn.Config(app, host='0.0.0.0', port=443, log_level="info", ssl_keyfile='ssl/myserver.key', ssl_certfile='ssl/myserver.csr')
+    config = uvicorn.Config(app, host='0.0.0.0', port=443, log_level="info", ssl_keyfile='ssl/key.pem', ssl_certfile='ssl/cert.pem')
     server = uvicorn.Server(config)
     await server.serve()
 
