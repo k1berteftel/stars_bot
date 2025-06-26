f24d40c2b405dbfe3ff3d95f8f90b3638b0de943
k1berteftel <kkulis985@gmail.com>
2025-06-26 23:27:19 +0300

    fix

--- utils/schedulers.py
+++ utils/schedulers.py
@@ -19,8 +19,8 @@ async def check_payment(bot: Bot, user_id: int, app_id: int, session: DataIntera
     order_id = kwargs.get('order_id')
     crypto_bot = await check_crypto_payment(invoice_id)
     crypto = await check_oxa_payment(track_id)
-    sbp = await check_p2p_sbp(order_id, card_id)
-    if crypto_bot or crypto or sbp:
+    #sbp = await check_p2p_sbp(order_id, card_id)
+    if crypto_bot or crypto: #or sbp:
         username = kwargs.get('username')
         stars = kwargs.get('stars')
         status = await transfer_stars(username, stars)
@@ -30,8 +30,8 @@ async def check_payment(bot: Bot, user_id: int, app_id: int, session: DataIntera
             payment = 'crypto_bot'
         if crypto:
             payment = 'crypto'
-        if sbp:
-            payment = 'sbp'
+        #if sbp:
+            #payment = 'sbp'
         if not status:
             await bot.send_message(
                 chat_id=user_id,
